600396cbe2b0fa4641a27d37779753afb722aa4a
iOSDevelopin <io@iosDevelop.in>
2015-04-01 13:09:36 -0700

    Committed by Udacity

---
+++ PitchShifter/RecordedAudio.swift
@@ -0,0 +1,15 @@
+//
+//  RecordedAudio.swift
+//  PitchShifter
+//
+//  Created by Cinema on 3/27/15.
+//  Copyright (c) 2015 iOSDevelop.in. All rights reserved.
+//
+
+import Foundation
+
+class RecordedAudio: NSObject{
+    var filePathUrl: NSURL!
+    var title: String!
+}
+
